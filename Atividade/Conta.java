f2b43eea3d307b161c39f6e3b433006f251b16e7
GuiAlbg <guilhermeargomess@gmail.com>
2024-03-14 18:34:36 -0300

    atividade 4

---
+++ Atividade/Conta.java
@@ -0,0 +1,33 @@
+package br.com.caelum.contas.modelo;
+public class Conta {
+    
+    int numero;
+    
+    String titular;
+
+    private static int totalDeContas;
+
+    double saldo;
+
+    Conta(String titular){
+        this.titular = titular;
+        adicionarConta();
+    }
+
+    Conta(int numero, String titular){
+        this(titular);
+        this.numero = numero;
+    }
+
+    private void adicionarConta(){
+        totalDeContas ++;
+    }
+
+    public static int getTotalDeContas(){
+        return totalDeContas;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+}
